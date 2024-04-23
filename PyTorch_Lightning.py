e2911d15c35c8c1ef6581c822dd09754156ee56d
RaminMod <111320451+RaminMod@users.noreply.github.com>
2024-04-23 22:47:10 +0200

    '.'

---
+++ PyTorch_Lightning.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from torch.utils.data import random_split
+
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+
