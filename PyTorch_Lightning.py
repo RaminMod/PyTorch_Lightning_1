81e111e8eafd8e37808bf0c173f02c4e8415a18b
RaminMod <111320451+RaminMod@users.noreply.github.com>
2024-04-23 23:50:14 +0200

    '.'

--- PyTorch_Lightning.py
+++ PyTorch_Lightning.py
@@ -6,17 +6,53 @@ from torch import nn, optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torch.utils.data import random_split
+import pytorch_lightning as pl
 
-class NN(nn.Module):
+    
+class NN(pl.LightningModule):
     def __init__(self, input_size, num_classes):
         super().__init__()
         self.fc1 = nn.Linear(input_size, 50)
         self.fc2 = nn.Linear(50, num_classes)
+        self.loss_fn = nn.CrossEntropyLoss()
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+    
+    def training_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log('val_loss', loss)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        loss, scores, y = self._common_step(batch, batch_idx)
+        self.log('test_loss', loss)
+        return loss
+    
+    def _common_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.size(0), -1)
+        scores = self.forward(x)
+        loss = F.loss_fn(scores, y)
+        return loss, scores, y
+    
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.reshape(x.size(0), -1)
+        scores = self.forward(x)
+        preds = torch.argmax(scores, dim=1)
+        return preds
+    
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=0.001)
+
     
 # Set device cuda for GPU if it is available otherwise use CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
